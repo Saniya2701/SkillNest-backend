d53ccd981cf6cf7c0fcdbd2d1f842eb0f0290bbe
Saniya2701 <185913688+Saniya2701@users.noreply.github.com>
2026-03-03 21:35:00 +0530

    Improve Email Verification

--- mail/templates/emailVerificationTemplate.js
+++ mail/templates/emailVerificationTemplate.js
@@ -69,7 +69,7 @@ const otpTemplate = (otp, name) => {
 	<body>
 		<div class="container">
 			<a href=""><img class="logo"
-					src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+					src="https://i.ibb.co/7Xyj3PC/logo.png" alt="SkillNest Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear ${name}</p>
